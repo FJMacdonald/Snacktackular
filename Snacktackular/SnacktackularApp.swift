0f93482f88da559ee5b33d829bfe4e7a916e9497
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-30 09:30:03 -0400

    Initial Commit

---
+++ Snacktackular/SnacktackularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktackularApp.swift
+//  Snacktackular
+//
+//  Created by Francesca MACDONALD on 2023-08-30.
+//
+
+import SwiftUI
+
+@main
+struct SnacktackularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
